685e2e63b5561fbb5ebed05a204a137d49cfe968
Dzhalil <220963@astanait.edu.kz>
2023-05-25 15:47:13 +0600

    Writing methods for MyGraph class

---
+++ src/MyGraph.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MyGraph <Vertex>{
+    private Map<Vertex, List<Vertex>> list;
+
+    public MyGraph(){
+        list = new HashMap<>();
+    }
+
+    public void addVertex(Vertex vertex){
+
+    }
+
+    public void addEdge(){
+
+    }
+
+    public void printGraph(){
+
+    }
+
+    public void removeEdge(){}
+
+    public boolean hasEdge(){
+        return false;
+    }
+
+    public List<Vertex> getNeighbours(Vertex vertex){
+        return null;
+    }
+
+    public void BFS(Vertex start){
+
+    }
+
+    public void  Djakstra(){}
+
+
+
+
+}
